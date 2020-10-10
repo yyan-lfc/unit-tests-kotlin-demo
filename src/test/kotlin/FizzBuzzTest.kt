f820885cf2d9833501d76747ad7e861fe7f9708b
Yan Yu <yanyulfc@163.com>
2020-10-10 13:11:51 +0800

    [YY] init demo with junit 5

---
+++ src/test/kotlin/FizzBuzzTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class FizzBuzzTest {
+    @Test
+    fun should_use_junit_5_and_pass() {
+        assertEquals(1, 1)
+    }
+}
